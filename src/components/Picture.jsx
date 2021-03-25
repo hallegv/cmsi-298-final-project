b62e7d67dcf0842c69e2ff9deea759d380de5827
hallegv <hallevogelpohl@gmail.com>
2021-03-25 11:19:35 -0700

    added alt attribute to img tag to get rid of compilation errors.

--- src/components/Picture.jsx
+++ src/components/Picture.jsx
@@ -4,7 +4,10 @@ import React from "react";
 export default function Picture() {
   return (
     <div>
-      <img src="https://images.unsplash.com/photo-1560114928-40f1f1eb26a0?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHw%3D&w=1000&q=80"/>
+      <img
+        src="https://images.unsplash.com/photo-1560114928-40f1f1eb26a0?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHw%3D&w=1000&q=80"
+        alt="kitten in grass"
+      />
     </div>
   );
 }
