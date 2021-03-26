a2d310fd6f217db90caf94480c106954ed7a7d68
Aidan Dionisio <aidanbcd@mac.com>
2021-03-25 21:20:58 -0700

    Added Router skeleton.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,64 @@
 import "./App.css";
-//import Game from "./components/Game";
+import Game from "./components/Game";
 import Picture from "./components/Picture";
+import HomePage from "./components/HomePage";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Picture />
-    </div>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              
+              <li>
+                <Link to="/play">Play</Link>
+              </li>
+
+              <li>
+                <Link to="/picture">Picture</Link>
+              </li>
+              
+            </ul>
+          </nav>
+
+          <Switch>
+          <Route path="/picture">
+              <Image />
+            </Route>
+            <Route path="/play">
+              <Play />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+          </div>
+      </Router>
+</div>
   );
   
+}
+
+function Home() {
+  return <HomePage />;
+}
+
+function Play() {
+  return <Game />;
+}
+
+function Image() {
+  return <Picture />;
 }
 
 export default App;
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-//import Button from 'react-bootstrap/Button';
+import Button from 'react-bootstrap/Button';
 //import Picture from "./components/Picture";
 
 export default function Game() {
