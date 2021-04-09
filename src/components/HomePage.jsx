10de08f6454544ce4287a3c9984e35f6833e4354
Aidan Dionisio <aidanbcd@mac.com>
2021-04-08 19:49:04 -0700

    Updated HomePage. Button not functional.

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,5 +1,26 @@
 import React from "react";
 
-export default function HomePage() {
-  return <h1>Welcome to the Game!</h1>;
+export default function HomePage(props) {
+
+  return <div>
+    <h1 style = {{
+      textAlign: "center",
+      fontFamily: "Papyrus",
+      fontSize: 75,
+      borderBottom: 0,
+      paddingBottom: 0,
+      marginBottom: 0}}
+      >Welcome to the Game!</h1>
+
+    <p>[GAME NAME HERE] is a game that tests how much you sweat the details!</p>
+    <p>Start with a super zoomed-in image and guess what it is!</p>
+
+    <button style = {{
+      width: "40%",
+      height: 100,
+      fontSize: 50,
+      fontFamily: "Monaco",
+    }}>Play!</button>
+  </div>;
+
 }
