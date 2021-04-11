af90cd5472fd7312048280fd1b9f4868442954c4
hallegv <hallevogelpohl@gmail.com>
2021-04-11 12:51:49 -0700

    Need to fix Error 401: Invalid Access Token.

--- src/components/Picture.jsx
+++ src/components/Picture.jsx
@@ -5,18 +5,18 @@ export default function Picture() {
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
   const [pics, setPics] = useState([]);
-  const url =
-    "https://api.shutterstock.com/v2/images/search?query=animals&image_type=photo";
+  const USER_ID = "cl2VWcsJbkCm52q4fYZEHxqhti7jWfkB";
+  const url = "https://api.shutterstock.com/v2/images/search?query=dog";
   useEffect(() => {
     fetch(url, {
       mode: "no-cors",
       method: "GET",
+      authorization:
+        "Bearer v2/Y2wyVldjc0pia0NtNTJxNGZZWkVIeHFodGk3aldma0IvMjk4NTQxMjc0L2N1c3RvbWVyLzQvUXd6aDVoUGg0MVlLQTdmeWpCYVJaUzVzYlAtRUNTQ045ZlhZR1JMT1lhMDFCSlhYT3hDX1ZSVTB3dnpUUUQyTTZoTUUwTHdLMDN3WllDTV9HTENBNGFDXzk0Z2V2amVHbWJhTm5GTFMwX1lWSkxsdE1aaUJIXzhSOHFDckZpd1ZGQWRiMXZ0XzBjMko4LVluUV90OVVGZmk3SHRsVGxDN1JVUEFuY3E5ZVJlT1lnNHFHV0Q5STZxTFpBcXBNM283WlhWOXpDakx3dWdLQnJMRjJZc3pTdy9tSG4wZWRMTFBPeHVfN1gwVmtXdWNB",
       headers: {
         // TODO: resolve 401 Invalid access token error
-        authorizaion: "Bearer Token" + pics?.id_token,
-        consumerKey: "cl2VWcsJbkCm52q4fYZEHxqhti7jWfkB",
-        consumerSecret: "RRwkRI85NGUllXzQ",
-        "Content-Type": "application/json",
+        user_id: USER_ID,
+        "User-Agent": "cmsi298finalproject",
       },
     })
       .then((res) => res.json())
