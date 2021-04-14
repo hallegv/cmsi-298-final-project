7f2cbff935c824fff9e1721697a53c0733fa3337
hallegv <hallevogelpohl@gmail.com>
2021-04-14 11:36:04 -0700

    fixed some errors in Picture.jsx

--- src/components/Picture.jsx
+++ src/components/Picture.jsx
@@ -1,39 +1,39 @@
 import { useState, useEffect } from "react";
-//import image from './images/image.jpg';
 
 export default function Picture() {
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
   const [pics, setPics] = useState([]);
-  const USER_ID = "cl2VWcsJbkCm52q4fYZEHxqhti7jWfkB";
-  const url = "https://api.shutterstock.com/v2/images/search?query=dog";
+  // const USER_ID = "cl2VWcsJbkCm52q4fYZEHxqhti7jWfkB";
+  // const url = "https://api.shutterstock.com/v2/images/search?query=dog";
+  const sstk = require("shutterstock-api");
+  const api = new sstk.ImagesApi();
+  const queryParams = {
+    query: "dogs",
+    sort: "popular",
+    orientation: "horizontal",
+  };
+
   useEffect(() => {
-    fetch(url, {
-      mode: "no-cors",
-      method: "GET",
-      authorization:
-        "Bearer v2/Y2wyVldjc0pia0NtNTJxNGZZWkVIeHFodGk3aldma0IvMjk4NTQxMjc0L2N1c3RvbWVyLzQvUXd6aDVoUGg0MVlLQTdmeWpCYVJaUzVzYlAtRUNTQ045ZlhZR1JMT1lhMDFCSlhYT3hDX1ZSVTB3dnpUUUQyTTZoTUUwTHdLMDN3WllDTV9HTENBNGFDXzk0Z2V2amVHbWJhTm5GTFMwX1lWSkxsdE1aaUJIXzhSOHFDckZpd1ZGQWRiMXZ0XzBjMko4LVluUV90OVVGZmk3SHRsVGxDN1JVUEFuY3E5ZVJlT1lnNHFHV0Q5STZxTFpBcXBNM283WlhWOXpDakx3dWdLQnJMRjJZc3pTdy9tSG4wZWRMTFBPeHVfN1gwVmtXdWNB",
-      headers: {
-        // TODO: resolve 401 Invalid access token error
-        user_id: USER_ID,
-        "User-Agent": "cmsi298finalproject",
-      },
-    })
-      .then((res) => res.json())
-      .then(
-        (result) => {
-          setIsLoaded(true);
-          setPics(result);
-          console.log(pics);
-        },
-        (error) => {
-          setIsLoaded(true);
-          setError(true);
-        }
-      );
-  }, [pics]);
+    sstk.setAccessToken(
+      "v2/Y2wyVldjc0pia0NtNTJxNGZZWkVIeHFodGk3aldma0IvMjk4NTQxMjc0L2N1c3RvbWVyLzQvUXd6aDVoUGg0MVlLQTdmeWpCYVJaUzVzYlAtRUNTQ045ZlhZR1JMT1lhMDFCSlhYT3hDX1ZSVTB3dnpUUUQyTTZoTUUwTHdLMDN3WllDTV9HTENBNGFDXzk0Z2V2amVHbWJhTm5GTFMwX1lWSkxsdE1aaUJIXzhSOHFDckZpd1ZGQWRiMXZ0XzBjMko4LVluUV90OVVGZmk3SHRsVGxDN1JVUEFuY3E5ZVJlT1lnNHFHV0Q5STZxTFpBcXBNM283WlhWOXpDakx3dWdLQnJMRjJZc3pTdy9tSG4wZWRMTFBPeHVfN1gwVmtXdWNB"
+    );
+    api
+      .searchImages(queryParams)
+      .then(function ({ data }) {
+        console.log(data);
+        setIsLoaded(true);
+        setPics(data);
+      })
+      .catch(function (error) {
+        console.error(error);
+        setIsLoaded(true);
+        setError(true);
+      });
+  });
 
   if (error) {
+    console.log(error);
     return <div>Error! Cannot display photo{error.message}</div>;
   } else if (!isLoaded) {
     return <div>Loading...</div>;
@@ -42,7 +42,7 @@ export default function Picture() {
       <ul>
         {pics.map((pic) => (
           <img key={pic.id} alt="Randomized for game">
-            {pic.preview_1000.url}
+            {pic.small_thumb}
           </img>
         ))}
       </ul>
