36f0362f20c494e0da05bcefd579d872714b93c5
Aidan Dionisio <aidanbcd@mac.com>
2021-04-22 19:48:11 -0700

    Respec'd App to consolidate homepage.

--- src/App.js
+++ src/App.js
@@ -1,42 +1,58 @@
 import "./App.css";
 import Picture from "./components/Picture";
-import HomePage from "./components/HomePage";
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   NavLink
 } from "react-router-dom";
-import { Button, ButtonGroup } from "react-bootstrap";
+import {useState, useEffect} from "react";
+import { Button, Spinner } from "react-bootstrap";
 
 function App() {
-  return (
-    <div className="App">
-      <Router>
-        <div>
-        <ButtonGroup toggle size="lg">
-            <NavLink to="/">
-              <Button variant="info">Home</Button>
-            </NavLink>
-
-            <NavLink to="/play">
-              <Button variant="success">Play</Button>
-            </NavLink>
-          </ButtonGroup>
-
-          <Switch>
-            <Route path="/play">
-              <Picture />
-            </Route>
-            <Route path="/">
-              <HomePage />
-            </Route>
-          </Switch>
-          </div>
-      </Router>
-</div>
-  );
+  const [playing, setPlaying] = useState(false);
+  const[loading, setLoading] = useState(false);
   
+  useEffect(() => {
+    setLoading(true);
+    setLoading(false);},
+    [setLoading]);
+    
+    if (loading) {
+      return <Spinner animation='border' style={{ margin: '50%' }} />;
+    }
+    else if(playing) {
+      return (<Picture />);
+    }
+    else {
+      return (
+        <div className="App">
+          <h1 style = {{
+            textAlign: "center",
+            fontFamily: "Papyrus",
+            fontSize: 75,
+            borderBottom: 0,
+            paddingBottom: 0,
+            marginBottom: 0}}
+          >Welcome to PicPredict!!</h1>
+          
+          <p>PicPredict is a game that tests how much you sweat the details!</p>
+          <p>Start with a super zoomed-in image and guess what it is!</p>
+          
+          <Router>
+            <NavLink to="/play">
+              <Button block style = {{fontFamily: "Monaco"}} onClick={() =>setPlaying(true)}>Play!</Button>
+            </NavLink>
+            
+            <Switch>
+              <Route path="/play">
+                <Picture />
+              </Route>
+            </Switch>
+          </Router>
+    </div>
+      );
+    }  
 }
 
 export default App;
