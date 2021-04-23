0628624e63660317664dc8fe71aa79f9128a283f
Aidan Dionisio <aidanbcd@mac.com>
2021-04-22 19:13:12 -0700

    Removed Game call in App.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import "./App.css";
-import Game from "./components/Game";
 import Picture from "./components/Picture";
 import HomePage from "./components/HomePage";
 import {
@@ -16,25 +15,18 @@ function App() {
       <Router>
         <div>
         <ButtonGroup toggle size="lg">
-            <NavLink to={'/'}>
+            <NavLink to="/">
               <Button variant="info">Home</Button>
             </NavLink>
 
-            <NavLink to={'/play'}>
+            <NavLink to="/play">
               <Button variant="success">Play</Button>
-            </NavLink>
-
-            <NavLink to="/picture">
-              <Button variant="light">Picture</Button>
             </NavLink>
           </ButtonGroup>
 
           <Switch>
-            <Route path="/picture">
-              <Picture />
-            </Route>
             <Route path="/play">
-              <Game />
+              <Picture />
             </Route>
             <Route path="/">
               <HomePage />
