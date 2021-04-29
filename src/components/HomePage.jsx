2f698b06fb8fc3566a920e1f1e04538c1a2be7f1
Aidan Dionisio <aidanbcd@mac.com>
2021-04-29 16:06:04 -0700

    Router functional.

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,35 +1,50 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink,
+} from "react-router-dom";
+import Picture from "./Picture"
 
-export default function HomePage(props) {
+export default function HomePage() {
+  const imgStyle = {
+    width: "20%",
+  }
+  
   return (
     <div>
-      <h1
-        style={{
-          textAlign: "center",
-          fontFamily: "Papyrus",
-          fontSize: 75,
-          borderBottom: 0,
-          paddingBottom: 0,
-          marginBottom: 0,
-        }}
-      >
-        Welcome to the Game!
-      </h1>
+      <Router>
+        <Switch>
+          <Route path="/play">
+            <Picture />
 
-      <p>
-        [GAME NAME HERE] is a game that tests how much you sweat the details!
-      </p>
-      <p>Start with a super zoomed-in image and guess what it is!</p>
+            <NavLink to="/">
+              <Button
+                block
+                style={{ fontFamily: "Monaco" }}
+                >
+                Back to home
+              </Button>
+            </NavLink>
+          </Route>
+          <Route path ="/">
+            <img src="https://i.imgur.com/kmlzo6m.png" alt = "b0rken" align="middle" style={imgStyle}></img>
+            <p>PicPredict is a game that tests how much you sweat the details!</p>
+            <p>Start with a super zoomed-in image and guess what it is!</p>
 
-      <Button
-        block
-        style={{
-          fontFamily: "Monaco",
-        }}
-      >
-        Play!
-      </Button>
+            <NavLink to="/play">
+              <Button
+                block
+                style={{ fontFamily: "Monaco" }}
+                >
+                Play!
+              </Button>
+            </NavLink>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
